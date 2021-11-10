5a3002a3f29154933a65d7209f2c775b25f513c1
aria-lopez <aria.lopez.dev@gmail.com>
2021-11-10 15:32:18 -0800

    Added file upload to s3 bucket with working lossy compression. This will not work for .gif files, so I need to add some logic to handle that case, as well as clean up some of the request logic

--- server/index.js
+++ server/index.js
@@ -6,16 +6,50 @@ const path = require('path');
 const Router = require('./router.js');
 const cors = require('cors');
 const multer = require('multer');
-const upload = multer({ dest: 'public' });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const fs = require('fs');
+const aws = require('aws-sdk');
+const sharp = require('sharp');
+const { accessKeyId, secretAccessKey } = require('./aws-config.js');
+
+const s3 = new aws.S3({
+    credentials: {
+        accessKeyId,
+        secretAccessKey
+    }
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.use(cors());
 
-app.post('/api/upload', upload.array('images', 10), (req, res, next) => {
+app.post('/api/upload', upload.array('images', 10), async (req, res) => {
+    const time = new Date();
+    fs.access('public', (err) => {
+        if (err) { fs.mkdirSync('public'); }
+    })
+    const { files } = req;
+    const imageLinks = [];
+    for (let i = 0; i < files.length; i++) {
+        const { buffer, originalname } = files[i];
 
-})
+        const compressedImage = await sharp(buffer)
+            .png({ quality: 20 })
+            .toBuffer();
+
+        const newRef = `${originalname.split('.')[0]}-${time.getTime()}.png`;
+        s3.putObject({
+            Bucket: 'imagez-storage',
+            Key: newRef,
+            Body: compressedImage,
+        }, (resp) => {
+            console.log(resp);
+        });     
+    }
+});
+
 
 
 app.listen(PORT, () => console.log(`App listening @ localhost:${PORT}`));
