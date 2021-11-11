ab75490e0f34457a6c6c38413a17e0dc2931ea05
aria-lopez <aria.lopez.dev@gmail.com>
2021-11-10 16:18:07 -0800

    Refactored Upload API to controllers file and fixed some logic, currently working as expected, still need to add error bounds for .gif files

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,46 @@
-const multer = require('multer');
-const upload = multer({ dest: 'public/'});
+const aws = require('aws-sdk');
+const sharp = require('sharp');
+const { accessKeyId, secretAccessKey } = require('../server/aws-config.js');
 
+const S3 = new aws.S3({
+    credentials: {
+        accessKeyId,
+        secretAccessKey,
+    }
+});
+
+const date = new Date();
 
 module.exports = {
-    handleImageUpload: (req, res) => {
+    handleImageUpload: async (req, res) => {
+        try {
+            const { files } = req;
+            const compressedImageLinks = [];
+            for (let i = 0; i < files.length; i++) {
+                const { buffer, originalname } = files[i];
+
+                const compressedBuffer = await sharp(buffer)
+                    .png({ quality: 30 })
+                    .toBuffer();
+
+                const newRef = `${originalname.split('.')[0]}-${date.getTime()}.png`;
+                
+                const imageLink = `https://imagez-storage.s3.us-west-1.amazonaws.com/${newRef}`;
+
+                S3.putObject({
+                    Bucket: 'imagez-storage',
+                    Key: newRef,
+                    Body: compressedBuffer,
+                }, (err, data) => {
+                    console.log(err);
+                    console.log(data);
+                });
+                compressedImageLinks.push({ link: imageLink, key: newRef });
+            }
+            res.status(201).json(compressedImageLinks);
+        } catch(e) {
+            console.log(e);
+            res.sendStatus(500);
+        }
     }
 }
--- server/router.js
+++ server/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 const { handleImageUpload } = require('../controllers');
 
-router.post('/image-upload', handleImageUpload);
+router.post('/upload', upload.array('images', 10), handleImageUpload);
 
 module.exports = router;
